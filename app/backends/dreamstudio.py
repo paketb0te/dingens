d904a19c29ad1f17a94df611c950c1a646df2f32
Manuel Stausberg <m.stausberg@pm.me>
2022-11-25 00:57:12 +0100

    make call to the stability.ai backend non-blocking

--- app/backends/dreamstudio.py
+++ app/backends/dreamstudio.py
@@ -1,6 +1,8 @@
+import asyncio
 import io
 import os
 import uuid
+from functools import partial
 from pathlib import Path
 
 import stability_sdk.client
@@ -26,22 +28,31 @@ class DreamstudioBackend:
     async def generate(self, user_input: UserInput) -> GeneratedOutput:
         prompt = self._transform_user_input(user_input=user_input)
 
-        # Set up our initial generation parameters.
-        answers = self.API.generate(
-            prompt=prompt,
-            seed=992446759,  # If a seed is provided, the resulting generated image will be deterministic.
-            # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
-            # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
-            steps=50,  # Step Count defaults to 50 if not specified here.
-            cfg_scale=8.0,  # Influences how strongly your generation is guided to match your prompt.
-            # Setting this value higher increases the strength in which it tries to match your prompt.
-            # Defaults to 7.0 if not specified.
-            width=512,  # Generation width, defaults to 512 if not included.
-            height=512,  # Generation height, defaults to 512 if not included.
-            samples=1,  # Number of images to generate, defaults to 1 if not included.
-            sampler=generation.SAMPLER_K_DPM_2_ANCESTRAL  # Choose which sampler we want to denoise our generation with.
-            # Defaults to k_lms if not specified. Clip Guidance only supports ancestral samplers.
-            # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_lms)
+        # Get the asyncio event loop
+        loop = asyncio.get_event_loop()
+        # Run the originally blocking function in a separate thread,
+        # scheduled by the default ThreadPoolExecutor ('None')
+        # Use functools.partial() to pass keyword variables into the scheduled function,
+        # See https://docs.python.org/3/library/asyncio-eventloop.html#asyncio-pass-keywords
+        answers = await loop.run_in_executor(
+            None,
+            partial(
+                self.API.generate,
+                prompt=prompt,
+                seed=992446759,  # If a seed is provided, the resulting generated image will be deterministic.
+                # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+                steps=50,  # Step Count defaults to 50 if not specified here.
+                cfg_scale=8.0,  # Influences how strongly your generation is guided to match your prompt.
+                # Setting this value higher increases the strength in which it tries to match your prompt.
+                # Defaults to 7.0 if not specified.
+                width=512,  # Generation width, defaults to 512 if not included.
+                height=512,  # Generation height, defaults to 512 if not included.
+                samples=1,  # Number of images to generate, defaults to 1 if not included.
+                sampler=generation.SAMPLER_K_DPM_2_ANCESTRAL  # Choose which sampler we want to denoise our generation with.
+                # Defaults to k_lms if not specified. Clip Guidance only supports ancestral samplers.
+                # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_lms)
+            ),
         )
 
         asset_urls = []
